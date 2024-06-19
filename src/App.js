015531db2e38a553d1d5524d84cf4fcfff75481f
Day Blassingame <dayblassingame@gmail.com>
2024-06-19 17:33:28 -0400

    styled header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.scss';
 import profilePic from './images/profileImg.jpg'
 import Header from "./Header";
 export default function App(){
--- src/Header.jsx
+++ src/Header.jsx
@@ -7,27 +7,29 @@ export default function Header({profileImg}){
     const [search, setSearch] = useState('');
 
     return(
-        <header>
-            <span id='start'>
-                <button><FontAwesomeIcon icon={faBars} alt='menu'/></button>
-                <a href='#'><img id='logo' alt="Youtube Home" title='Youtube Home' src={Logo}/></a>
-            </span>
-            
-            <span id='center'>
-                <input id='searchInput' type="text" value={search} onChange={(e)=> setSearch(e.target.value)}/>
-                <button id='searchBtn' alt='search' title='Search'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
-                <button id='voiceSearch' alt='search with your voice' title='Search with your voice'><FontAwesomeIcon icon={faMicrophone}/></button>
-            </span>
+        <div className='yt-c-header_wrapper'>
+            <header className='yt-c-header'>
+                <div id='start'>
+                    <button><FontAwesomeIcon id='icon' icon={faBars} alt='menu'/></button>
+                    <a href='#'><img id='logo' alt="Youtube Home" title='Youtube Home' src={Logo}/></a>
+                </div>
+                
+                <div id='center'>
+                    <input id='searchInput' type="text" value={search} placeholder='Search' onChange={(e)=> setSearch(e.target.value)} />
+                    <button id='searchBtn' alt='search' title='Search'><FontAwesomeIcon id='icon' icon={faMagnifyingGlass}/></button>
+                    <button id='voiceSearch' alt='search with your voice' title='Search with your voice'><FontAwesomeIcon id='icon' icon={faMicrophone}/></button>
+                </div>
 
-            <span id='end'>
-                <button><FontAwesomeIcon icon={faVideo} alt='create' title='Create'/></button>
-                <ul>
-                    <li alt='upload video'><a><FontAwesomeIcon icon={faPlay} />Upload Video</a></li>
-                    <li alt='go live'><a><FontAwesomeIcon icon={faTowerBroadcast}/> Go Live</a></li>
-                </ul>
-                <button><FontAwesomeIcon icon={faBell} alt='notifications' title='Notifications'/></button>
-                <button><img src={profileImg} alt='profile picture'/></button>
-            </span>
-        </header>
+                <div id='end'>
+                    <button><FontAwesomeIcon id='icon' icon={faVideo} alt='create' title='Create'/></button>
+                    <ul className="display_none">
+                        <li alt='upload video'><a><FontAwesomeIcon id='icon' icon={faPlay} />Upload Video</a></li>
+                        <li alt='go live'><a><FontAwesomeIcon id='icon' icon={faTowerBroadcast}/> Go Live</a></li>
+                    </ul>
+                    <button><FontAwesomeIcon id='icon' icon={faBell} alt='notifications' title='Notifications'/></button>
+                    <button><img id='profileimg' src={profileImg} alt='profile picture'/></button>
+                </div>
+            </header>
+        </div>
     )
 }
