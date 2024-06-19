e1742b54a6a57be92a24762bdd66322db8720f6a
Day Blassingame <dayblassingame@gmail.com>
2024-06-19 14:08:15 -0400

    added icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from './images/youtubeLogo.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faMicrophone, faMagnifyingGlass, faVideo, faBell} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header({profileImg}){
+    const [search, setSearch] = useState('');
 
     return(
         <header>
             <span id='start'>
-                <button>Hamburger Menu</button>
+                <button><FontAwesomeIcon icon={faBars}/></button>
                 <img id='logo' alt="Youtube Logo" src={Logo}/>
             </span>
             
             <span id='center'>
-                <input id='searchInput' type="text" />
-                <button id='searchBtn'>Search Icon</button>
-                <button id='voiceSearch'>mic</button>
+                <input id='searchInput' type="text" value={search} onChange={(e)=> setSearch(e.target.value)}/>
+                <button id='searchBtn'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
+                <button id='voiceSearch'><FontAwesomeIcon icon={faMicrophone}/></button>
             </span>
 
             <span id='end'>
-                <button>vid</button>
-                <button>Notifications</button>
-                <button>my profile</button>
+                <button><FontAwesomeIcon icon={faVideo}/></button>
+                <button><FontAwesomeIcon icon={faBell}/></button>
+                <button><img src={profileImg} alt='profilePic'/></button>
             </span>
         </header>
     )
